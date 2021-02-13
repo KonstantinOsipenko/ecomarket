fb8da9983ddc403460653190d344548fed782343
kostya <samuro9130@gmail.com>
2021-02-14 00:58:10 +0200

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+let changecolor;
+
+document.querySelector('.search').addEventListener('click', e => {
+    if (changecolor = e.target)
+        e.target.style.background = ' rgba(17, 17, 17, 0.08)';
+}
+);
