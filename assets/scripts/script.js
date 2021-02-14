87e4b8d906ab2a897a98b460dad687396ef83bb8
kostya <samuro9130@gmail.com>
2021-02-14 17:53:01 +0200

    some edit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,4 @@
 let changecolor;
-
 document.querySelector('.search').addEventListener('click', e => {
     if (changecolor = e.target)
         e.target.style.background = ' rgba(17, 17, 17, 0.08)';
