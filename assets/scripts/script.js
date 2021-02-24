4cf4421e787222689cbdeda5a611f8bffb918831
kostya <samuro9130@gmail.com>
2021-02-24 22:24:18 +0200

    new edit all page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
-let changecolor;
-document.querySelector('.search').addEventListener('click', e => {
-    if (changecolor = e.target)
-        e.target.style.background = ' rgba(17, 17, 17, 0.08)';
-}
-);
+// let changecolor;
+// document.querySelector('.search').addEventListener('click', e => {
+//     if (changecolor = e.target)
+//         e.target.style.background = ' rgba(17, 17, 17, 0.08)';
+// }
+// );
